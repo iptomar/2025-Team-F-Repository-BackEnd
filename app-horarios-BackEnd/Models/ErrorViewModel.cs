06b60d74f70c12fb8ae9160b4e67e56b85d235ea
Cristiane Mayabanza <152671012+itscristianee@users.noreply.github.com>
2025-04-07 01:14:23 +0100

    setup inicial com ASP.NET MVC

---
+++ app-horarios-BackEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace App_horarios_BackEnd.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
