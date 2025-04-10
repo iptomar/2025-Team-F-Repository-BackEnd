04a6b2466b2a1954d770eba6b8944ee26589937f
Cristiane Mayabanza <152671012+itscristianee@users.noreply.github.com>
2025-04-10 09:02:21 +0100

    Backend visual

--- app-horarios-BackEnd/Controllers/ImportVisualController.cs
+++ app-horarios-BackEnd/Controllers/ImportVisualController.cs
@@ -1,12 +1,37 @@
+using app_horarios_BackEnd.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace app_horarios_BackEnd.Controllers
 {
     public class ImportVisualController : Controller
     {
+        
+        private readonly ExcelImportService _importService;
+
+        public ImportVisualController(ExcelImportService importService)
+        {
+            _importService = importService;
+        }
+        
         // GET: ImportVisualController
         public ActionResult Index()
         {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                ViewBag.Message = "Ficheiro inválido.";
+                return View();
+            }
+
+            using var stream = file.OpenReadStream();
+            await _importService.ImportAllSheetsAsync(stream);
+
+            ViewBag.Message = "Importação concluída com sucesso!";
             return View();
         }
 
--- app-horarios-BackEnd/Program.cs
+++ app-horarios-BackEnd/Program.cs
@@ -5,28 +5,49 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRazorPages();
+builder.Services.AddSession();
+builder.Services.AddDbContext<HorarioDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors("AllowAll");
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseSession();
+app.MapRazorPages();
+
+// Configure endpoints for controllers and Razor Pages
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllers();
 
+
 app.Run();
 
-builder.Services.AddDbContext<HorarioDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
