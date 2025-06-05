707926129776765dd42bfc190b51488d4f0ff3c8
Cristiane Mayabanza <152671012+itscristianee@users.noreply.github.com>
2025-06-05 20:37:13 +0100

    Adicionar atributo na tabela Escola

---
+++ App-horarios-BackEnd/Migrations/20250605193452_AddAtributo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace app_horarios_BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAtributo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Abreviacao",
+                table: "Escolas",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Abreviacao",
+                table: "Escolas");
+        }
+    }
+}
